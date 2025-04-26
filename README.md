59b5d1567134d5a75cc82d03dc693aff82160138
livalves <liviamabelle@gmail.com>
2025-04-26 20:08:31 -0300

    Finalizando front tela de login

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# React + Vite
+# Sispar
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Aplicação web desenvolvida como desafio final do curso de Desenvolvimento Full Stack da Vai na Web.  
+O **Sispar** é uma remodelação do sistema utilizado pela Wilson Sons, com foco em modernizar a experiência do usuário e otimizar funcionalidades.
 
-Currently, two official plugins are available:
+## Tecnologias 
+- React (Vite)
+- HTML5 e SASS (pré-processador CSS)
+- JavaScript
+- Python (para o backend)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+## Objetivo
+- Melhorar a interface e usabilidade do sistema original.
+- Atualizar tecnologias utilizadas na aplicação.
+- Implementar melhores práticas de desenvolvimento web.
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,4 @@
 import {useNavigate} from "react-router-dom"
-//import Capa from "../../assets/Tela Login/imagem tela de login.png";
 import Logo from "../../assets/Tela Login/logo-ws.png";
 import styles from "./Login.module.scss"
 
@@ -8,42 +7,39 @@ function Login() {
 const navigate = useNavigate() //Iniciando o hook useNavigate
 
 const irParaReembolsos = () => {
-  navigate("/reembolsos")  //Redirecionando para a página de reembolsos
+    navigate("/reembolsos")  //Redirecionando para a página de reembolsos
 }
 
   return (
     <main className={styles.mainLogin}>
-      <section className={styles.containerFoto}>
-        {/* <img src={Capa} alt="Foto de um navio cargueiro" /> */}
-      </section>
+        <section className={styles.containerFoto}></section>
 
-      <section className={styles.formWapper}>
-        <div className={styles.boxLogo}>
-          <img src={Logo} alt="Logo da wilson sons" />
-          <h1>Boas vindas ao Novo Portal SISPAR </h1>
-          <p>Sistema de Emissão de Boletos e Parcelamento</p>
-        </div>
+        <section className={styles.formWapper}>
+            <div className={styles.boxLogo}>
+                <img src={Logo} alt="Logo da wilson sons" />
+                <h1>Boas vindas ao <br /> Novo Portal SISPAR </h1>
+                <p>Sistema de Emissão de Boletos e Parcelamento</p>
+            </div>
 
-        <form action="">
-          <input type="email" name="email" id="email" placeholder="Email" />
+            <form action="">
+                <input type="email" name="email" id="email" placeholder="Email" />
+                <input type="password" name="password" id="password" placeholder="Senha"/>
 
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Senha"
-          />
+                <button className={styles.btnRecoveryPass}> Esqueci minha senha</button>
 
-          <p>Esqueci minha senha</p>
+                <div className={styles.boxButton}>
+                    <button className={styles.btnSignIn} onClick={irParaReembolsos}> 
+                        <span> Entrar </span> 
+                    </button>
+                    <button className={styles.btnSignUp}> 
+                        <span> Criar conta </span> 
+                    </button>
+                </div>
 
-          <div className={styles.boxButton}>
-            <button onClick={irParaReembolsos}>Entrar</button>
-            <button>Criar conta</button>
-          </div>
-
-        </form>
-      </section>
+            </form>
+        </section>
     </main>
   );
 }
+
 export default Login;
